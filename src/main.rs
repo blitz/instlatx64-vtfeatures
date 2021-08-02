39f4a888264920c074063f7b0e267d6b74ea2d52
Julian Stecklina <julian.stecklina@cyberus-technology.de>
2021-08-02 11:58:08 +0200

    Refactor and add new features to parse from CPUID dumps

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
-#[macro_use] extern crate lazy_static;
+#[macro_use]
+extern crate lazy_static;
 
 use regex::Regex;
 use std::error;
-use std::io::BufRead;
 use std::io;
+use std::io::BufRead;
 
 type Result<T> = std::result::Result<T, Box<dyn error::Error>>;
 
@@ -17,25 +18,31 @@ struct MsrValue {
 // MSR 0000048B: 025F-7FFF-0000-0000
 //
 // If parsing is unsuccessful, `None` is returned.
-fn try_parse_msr_line(s: &str) -> Option<MsrValue>
-{
+fn try_parse_msr_line(s: &str) -> Option<MsrValue> {
     lazy_static! {
-	static ref RE: Regex = Regex::new(r"^MSR ([0-9a-fA-F]+): ([-0-9a-fA-F]+)$").expect("a valid regex");
+        static ref RE: Regex =
+            Regex::new(r"^MSR ([0-9a-fA-F]+): ([-0-9a-fA-F]+)$").expect("a valid regex");
     }
 
-    RE
-	.captures(s)
-	.map(|c|
-	     Some(MsrValue {
-		 index: u32::from_str_radix(c.get(1).unwrap().as_str(), 16).ok()?,
+    RE.captures(s)
+        .map(|c| {
+            Some(MsrValue {
+                index: u32::from_str_radix(c.get(1).unwrap().as_str(), 16).ok()?,
 
-		 // Values have to have their hyphens removed for parsing.
-		 value: u64::from_str_radix(&c.get(2).unwrap().as_str()
-					    .chars()
-					    .filter(|&c| c != '-')
-					    .collect::<String>(), 16).ok()?
-	     }))
-	.flatten()
+                // Values have to have their hyphens removed for parsing.
+                value: u64::from_str_radix(
+                    &c.get(2)
+                        .unwrap()
+                        .as_str()
+                        .chars()
+                        .filter(|&c| c != '-')
+                        .collect::<String>(),
+                    16,
+                )
+                .ok()?,
+            })
+        })
+        .flatten()
 }
 
 struct MsrMatch {
@@ -48,80 +55,96 @@ struct Feature {
     must_match: &'static [MsrMatch],
 }
 
-fn does_match(msr_values: &[MsrValue], msr_match: &MsrMatch) -> Option<bool>
-{
+fn does_match(msr_values: &[MsrValue], msr_match: &MsrMatch) -> Option<bool> {
     msr_values
-	.iter()
-	.find(|&m| m.index == msr_match.index)
-	.map(|m| (m.value & msr_match.must_be_set) == msr_match.must_be_set)
+        .iter()
+        .find(|&m| m.index == msr_match.index)
+        .map(|m| (m.value & msr_match.must_be_set) == msr_match.must_be_set)
 }
 
 // Checks whether a feature is available. The answer might be unknown,
 // if the relevant MSRs are not available.
-fn has_feature(msr_values: &[MsrValue], feature: &Feature) -> Option<bool>
-{
-    feature.must_match
-	.iter()
-	.map(|m| does_match(msr_values, m))
-	.fold(Some(true), |acc, n| acc.and_then(|b| match n {
-	    Some(c) => Some(b && c),
-	    None => None
-	}))
+fn has_feature(msr_values: &[MsrValue], feature: &Feature) -> Option<bool> {
+    feature
+        .must_match
+        .iter()
+        .map(|m| does_match(msr_values, m))
+        .fold(Some(true), |acc, n| acc.and_then(|b| n.map(|c| b && c)))
 }
 
-fn tristate_to_char(tristate: Option<bool>) -> char
-{
+fn tristate_to_char(tristate: Option<bool>) -> char {
     match tristate {
-	Some(b) => if b { 'Y' } else { 'N' },
-	None => '?'
+        Some(b) => {
+            if b {
+                'Y'
+            } else {
+                'N'
+            }
+        }
+        None => '?',
     }
 }
 
 static FEATURES: &[Feature] = &[
     Feature {
-	name: "VMCS Shadowing              ",
-	must_match: &[MsrMatch {
-	    index: 0x48b,
-	    must_be_set: 1 << 46,
-	}]
+        name: "EPT                         ",
+        must_match: &[MsrMatch {
+            index: 0x48b,
+            must_be_set: 1 << (32 + 1),
+        }],
     },
-
     Feature {
-	name: "APIC-register virtualization",
-	must_match: &[MsrMatch {
-	    index: 0x48b,
-	    must_be_set: 1 << 40,
-	}]
+        name: "Unrestricted Guest          ",
+        must_match: &[MsrMatch {
+            index: 0x48b,
+            must_be_set: 1 << (32 + 7),
+        }],
     },
-
     Feature {
-	name: "Virtual-interrupt delivery  ",
-	must_match: &[MsrMatch {
-	    index: 0x48b,
-	    must_be_set: 1 << 41,
-	}]
+        name: "VMCS Shadowing              ",
+        must_match: &[MsrMatch {
+            index: 0x48b,
+            must_be_set: 1 << 46,
+        }],
     },
-
     Feature {
-	name: "Process posted interrupts   ",
-	must_match: &[MsrMatch {
-	    index: 0x481,
-	    must_be_set: 1 << (7 + 32),
-	}]
-    }
+        name: "APIC-register virtualization",
+        must_match: &[MsrMatch {
+            index: 0x48b,
+            must_be_set: 1 << 40,
+        }],
+    },
+    Feature {
+        name: "Virtual-interrupt delivery  ",
+        must_match: &[MsrMatch {
+            index: 0x48b,
+            must_be_set: 1 << 41,
+        }],
+    },
+    Feature {
+        name: "Process posted interrupts   ",
+        must_match: &[MsrMatch {
+            index: 0x481,
+            must_be_set: 1 << (7 + 32),
+        }],
+    },
 ];
 
 fn main() -> Result<()> {
     let msr_values = io::stdin()
-	.lock()
-	.lines()
-	.collect::<io::Result<Vec<String>>>()?
-	.iter()
-	.filter_map(|l| try_parse_msr_line(l))
-	.collect::<Vec<MsrValue>>();
+        .lock()
+        .lines()
+        .collect::<io::Result<Vec<String>>>()?
+        .iter()
+        .filter_map(|l| try_parse_msr_line(l))
+        .collect::<Vec<MsrValue>>();
 
     for feature in FEATURES {
-	println!("{}: {}", feature.name, tristate_to_char(has_feature(&msr_values, feature)));
+        println!(
+            "{}: {}",
+            feature.name,
+            tristate_to_char(has_feature(&msr_values, feature))
+        );
     }
 
     Ok(())
